47dd663b770ad6332854f605ed2d0340e98c8b24
maziTR <mazi.tr@gmail.com>
2017-05-19 10:13:07 +0300

    g

--- scripts/main.js
+++ scripts/main.js
@@ -5,22 +5,31 @@ var consume = "";
 var currQ = 0;
 
 $(".queOptions").click(function () {
+  var _thePQ = ($(this).parent());
   if ($(this).hasClass("correct"))
   {
     $(this).addClass("right");
     setTimeout(function () {
-    countMePoints+=1; }, 1000);
+    countMePoints+=1; }, 100);
+    $('#mePoints').text(countMePoints);
   }
   else
   {
     $(this).addClass("wrong");
     setTimeout(function () {
-      countYouPoint+=1; }, 1000);
+        ($(this).parent()).find('.queOptions .correct').addClass("notRight");
+      countYouPoint+=1; }, 50);
+
+      $('#youPoints').text(countYouPoint);
   }
 
+<<<<<<< Updated upstream
   $('#mePoints').text(countMePoints);
   $('#youPoints').text(countYouPoint);
   $('#laTimer').text('0');
+=======
+
+>>>>>>> Stashed changes
 
 
 
@@ -28,11 +37,13 @@ $(".queOptions").click(function () {
   currQ = ($(this).parent()).index();
   currQ+=1;
 
+
 //chack ans
 setInterval(function () {
   $(".question").addClass("hidden");
+
   $(".question[data-id|=" + currQ + "]").removeClass("hidden");
-}, 1000);
+}, 700);
 //show feedback
 
 
